cbf4434c9d2c6c8acf6682536c40bdb5b110192f
XRL <mariusgava@yahoo.com>
2023-03-28 01:49:28 +0300

    added signin and signup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Sign } from "./routes/Sign";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return(
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/sign' element={<Sign />} />
+            </Routes>
+        </Router>
+    )
 }
-
-export default App;
